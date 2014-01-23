b748f0c8eed5594630005c6539635da6b2d80d62
anreese@ucsd.edu <vagrant@precise32.(none)>
2014-01-23 19:09:21 +0000

    javascript lab 3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,40 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("I luv you!");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
 
+
+	$("a.thumbnail").click(projectClick);
+	$("#submitBtn").click(updateProject);
+}
+
+function updateProject(e) {
+   var projectID = $('#project').val();
+   $(projectID).animate({
+      width: $('#width').val()
+   });
+
+   var newText = $('#description').val();
+   $(projectID + " .project-description").text(newText);
+}
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+function projectClick(e) {
+//prevent the page from reloading
+	e.preventDefault();
+	$(this).css("background-color", "#7fff00")
+
+
+
+	var containingProject = $(this).closest(".project"); 
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) { 
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
+    } else { 
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
 }
